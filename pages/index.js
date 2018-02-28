09af52ac7176d343bde4506a055dbb12a0ebd0bd
Maksim Korzhov <korzhov.m.s@yandex.ru>
2018-02-28 19:05:13 +0300

    Add dynamic page urls only on client side

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Link from "next/link";
 import Layout from "../components/Layout";
+
+const PostTitle = (props) => (
+    <li>
+        <Link as={ `/p/${ props.id }` } href={ `/post?title=${ props.title }` }>
+            <a>{ props.title }</a>
+        </Link>
+    </li>
+);
 
 const Index = () => (
     <Layout>
-        <p>Hello Next.js</p>
+        <h1>My Blog</h1>
+        <ul>
+            <PostTitle id="hello-nextjs" title="Hello, Next.js" />
+            <PostTitle id="learn-nextjs" title="Learn Next.js is awesome" />
+            <PostTitle id="deploy-nextjs" title="Deploy apps with Zelt" />
+        </ul>
     </Layout>
 );
 
